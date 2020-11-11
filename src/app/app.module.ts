8b1751e02367e17cc144664380846d6edf2dad6f
HugoCastillo <hugo.somnium@gmail.com>
2020-11-11 09:20:27 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import {APP_ROUTING} from './app.routers'
+
+//Serivios
+import {HeroesService} from './servicios/heroes.service'
+
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { from } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,9 +25,13 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/servicios/heroes.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core'
+
+@Injectable()
+export class HeroesService{
+
+    constructor(){
+        console.log("Servicio listo para usar!!!")
+    }
+ 
+    getHeroes(){
+        return this.getHeroes;
+    }
+
+}
